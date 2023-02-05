023ac4e898443a2942cc7471d6095eebaef20127
Martin M <m.marcos25@hotmail.com>
2023-02-05 13:12:34 -0500

    a
    a

--- Frontend/jobster/src/App.js
+++ Frontend/jobster/src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 
-function App() {
+import { Component } from 'react';
+import Greet1 from './components/greet'
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          JOBSTER
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <>
+    <Router>
+      <Navbar/>
+      <Routes>
+
+        <Route path ='/' exact/>
+      </Routes>
+
+    </Router>
+  </>
+
+
+);
+  }
 
 export default App;
---
+++ Frontend/jobster/src/components/greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Greet2(){
+
+    return <h1>Hello bro2</h1>
+}
+
+export default Greet2
