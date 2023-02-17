cce011d5a30b1c1fc9ab0d2ae546859910f99971
krishnnas <krishnnas@outlook.com>
2023-02-17 10:57:12 -0500

    add event form created

---
+++ app/views/Event/AddEvent.php
@@ -0,0 +1,24 @@
+<?php include 'app/views/Common/header.php' ?>
+
+    <div class="content">
+    <form method="post">
+        <h1>Add Event</h1>
+        <br/>
+        <h3 class="mb-4 font-weight-normal">Event info</h3>
+
+        <div class="form-label-group">
+            <input type="text" name="name" class="form-control" id="name" placeholder="Event Name" required/>
+            <label for="name">* Event Name</label>
+        </div>
+
+        <div class="form-label-group">
+            <input type="text" name="descr" class="form-control" id="descr" placeholder="Description" required/>
+            <label for="descr">* Event Description</label>
+        </div>
+
+        <input type="submit" name="action" value="Create Group" class="btn btn-lg btn-primary btn-block"/>
+        <input type="submit" name="cancel" value="Cancel" class="btn btn-lg btn-primary btn-block"/>
+    </form>
+    </div>
+
+<?php include 'app/views/Common/footer.php' ?>
